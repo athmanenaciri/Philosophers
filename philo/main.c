e50be38d3802395577f7f22ee01080c1f3152e00
Athmane Naciri <anaciri@e3r8p8.1337.ma>
2022-06-17 16:04:02 +0100

    philo

---
+++ philo/main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaciri <anaciri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 15:11:51 by anaciri           #+#    #+#             */
+/*   Updated: 2022/06/17 15:59:59 by anaciri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int main(int ac, char **av)
+{
+	t_data data;
+	
+	while(ac > 6 || ac < 5)
+		return(1);
+	if(ft_atoi(av[1], &data.num_of_philo))
+		return(1);
+	if(ft_atoi(av[2], &data.time_to_die))
+		return(1);
+	if(ft_atoi(av[3], &data.time_to_eat))
+		return(1);
+	if(ft_atoi(av[4], &data.time_to_sleep))
+		return(1);
+	if(ac == 5)
+		data.num_of_meals = -1;
+	
+	printf("%d", data.num_of_meals);
+	else
+	{
+		if(ft_atoi(av[5], &data.num_of_meals))
+			return(1);
+	}
+
+
+
+
+
+
+
+		
+	return(0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaciri <anaciri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 15:10:35 by anaciri           #+#    #+#             */
+/*   Updated: 2022/06/17 15:32:22 by anaciri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdlib.h>
+# include <unistd.h>
+#include<stdio.h>
+
+typedef struct node
+{
+	int	num_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_of_meals;
+}t_data;
+
+int	ft_atoi(char *str, int *nbr);
+
+#endif
