bfd9f4428f9714977d3f974c408fd36f856d8a81
Athmane Naciri <anaciri@e3r7p4.1337.ma>
2022-07-19 18:10:55 +0100

    Fork

--- philo/generate_threads.c
+++ philo/generate_threads.c
@@ -5,16 +5,28 @@ void	*routine(void *ptr)
 	t_philo	*philo;
 
 	philo = (t_philo*)ptr;
-
+	if((philo->nbr % 2) == 0)
+		usleep(100);
 	while(1)
 	{
+		printf(" %d is thinking\n", philo->nbr);
+		
+		pthread_mutex_lock(&philo->left);
 		printf(" %d has taken a fork\n" ,philo->nbr);
+
+		pthread_mutex_lock(philo->right);
+		printf(" %d has taken a fork\n" ,philo->nbr);
+
 		printf(" %d is eating\n" ,philo->nbr);
 		usleep(philo->data->time_to_eat * 1000);
+
+		pthread_mutex_unlock(&philo->left);
+		pthread_mutex_unlock(philo->right);
+
 		printf(" %d is sleeping\n" ,philo->nbr);
 		usleep(philo->data->time_to_sleep * 1000);
-		printf(" %d is thinking\n", philo->nbr);
-		printf(" %d died\n", philo->nbr);
+		
+		//printf(" %d died\n", philo->nbr);
 	}
 	return(NULL);
 }
@@ -42,11 +54,16 @@ int	create_philo(t_data *data)
 		else
 			philos[i].right = &philos[philos->data->num_of_philo - 1].left;
 		//protect thread 
-		pthread_create(&philos[i].thread, NULL, routine, &philos[i]);
 
+		i++;
+	}
+	i= 0;
+	while(i < num)
+	{
+		pthread_create(&philos[i].thread, NULL, routine, &philos[i]);
 		i++;
 	}
 	return(0);
 }
 
-//int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_lock(pthread_mutex_t *mutex);
