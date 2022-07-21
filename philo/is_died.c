6e9d82de05d3a3e68b45a501a3d9186baeefd960
Ismail Ait bella <iait-bel@e3r1p12.1337.ma>
2022-07-21 20:22:26 +0000

    Fork

--- philo/is_died.c
+++ philo/is_died.c
@@ -6,7 +6,7 @@
 /*   By: iait-bel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 16:44:35 by iait-bel          #+#    #+#             */
-/*   Updated: 2022/07/21 17:55:57 by iait-bel         ###   ########.fr       */
+/*   Updated: 2022/07/21 20:00:10 by iait-bel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int is_died(t_philo *philos, t_data *data)
 			printf("died");
 			return(1);
 		}
-	//	printf("#%lu#\n", philos[i].last_meal);
+		printf("#%lu#\n", philos[i].last_meal);
 		//printf("(%lu)", (philos[i].last_meal - my_time()));
 		i++;
 	}
